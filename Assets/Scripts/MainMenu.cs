ec1782d5539efe356ff8a941b158865c4396455c
Brandon Gomez <brlgomez@ucsc.edu>
2016-09-10 20:55:13 -0700

    Added the frameworks for a store in the main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,14 +8,20 @@ public class MainMenu : MonoBehaviour {
 	public Button playButton;
 	public Button playBowlingButton;
 	public Button playDriveButton;
+	public Button storeButton;
 	public Button settingsButton;
+	public Button sudanButton;
+	public Button limoButton;
 	public Button soundButton;
 	public Button musicButton;
 	public Button vibrationButton;
 
+	public Text title;
 	public Text floorItText;
 	public Text bowlingText;
 	public Text driveText;
+	public Text storeText;
+	public Text settingsText;
 	public Text soundText;
 	public Text musicText;
 	public Text vibrationText;
@@ -23,12 +29,15 @@ public class MainMenu : MonoBehaviour {
 	public Text bowlingHighScore;
 	public Text drivingHighScore;
 	public Text cashText;
+
 	int highScoreInfinite;
 	int highScoreBowling;
 	int highScoreDriving;
 	int cash;
+	int carNumber;
 
 	bool viewSettings;
+	bool viewStore;
 	public Text loadingText;
 	bool loading;
 
@@ -37,17 +46,23 @@ public class MainMenu : MonoBehaviour {
 	Vector4 textOn;
 	Vector4 textOff;
 
+	public Mesh[] carMeshes;
+
 	void Start () {
 		playButton.onClick.AddListener(delegate { playButtonClick(); });
 		playBowlingButton.onClick.AddListener(delegate { playBowlingButtonClick(); });
 		playDriveButton.onClick.AddListener(delegate { playDrivingButtonClick(); });
+		storeButton.onClick.AddListener (delegate { storeButtonClick (); });
 		settingsButton.onClick.AddListener (delegate { settingsButtonClick (); });
 		soundButton.onClick.AddListener (delegate { soundEffectsButtonClick (); });
 		musicButton.onClick.AddListener (delegate { musicButtonClick (); });
 		vibrationButton.onClick.AddListener (delegate { vibrationButtonClick (); });
+		sudanButton.onClick.AddListener (delegate { sudanButtonClick (); });
+		limoButton.onClick.AddListener (delegate { limoButtonClick (); });
 		highScoreInfinite = PlayerPrefs.GetInt ("High Score Infinite", 0);
 		highScoreBowling = PlayerPrefs.GetInt ("High Score Bowling", 0);
 		highScoreDriving = PlayerPrefs.GetInt ("High Score Driving", 0);
+		carNumber = PlayerPrefs.GetInt ("Car Number", 0);
 		cash = PlayerPrefs.GetInt ("Cash", 0);
 		highScore.text = "High Score " + highScoreInfinite;
 		bowlingHighScore.text = "High Score " + highScoreBowling;
@@ -58,7 +73,9 @@ public class MainMenu : MonoBehaviour {
 		textOn = new Vector4 (1, 1, 1, 1);
 		textOff = new Vector4 (1, 1, 1, 0);
 		viewSettings = false;
-		settingsOff ();
+		viewStore = false;
+		menuOn ();
+		GameObject.Find ("Car").GetComponent<MeshFilter> ().mesh = carMeshes[carNumber];
 		cashText.text = "Cash " + cash;
 		if (PlayerPrefs.GetInt ("Play Sound Effects", 0) == 0){
 			soundText.text = "Sound Effects: On";
@@ -77,52 +94,10 @@ public class MainMenu : MonoBehaviour {
 			vibrationText.text = "Vibration: Off";
 		}
 	}
-
-	void Update(){
-		if (Input.GetMouseButtonUp (0)) {
-			if (!viewSettings && !playButton.GetComponent<Button> ().enabled) {
-				settingsOff ();
-			} else if (viewSettings && playButton.GetComponent<Button> ().enabled) {
-				settingsOn ();
-			}
-		}
-		/*
-		if (Input.GetKeyDown("space")) {
-			PlayerPrefs.DeleteAll();
-		}
-		*/
-	}
-
-	void settingsOn(){
-		playButton.GetComponent<Button> ().enabled = false;
-		playButton.GetComponent<Image> ().color = buttonOff;
-		floorItText.GetComponent<Text> ().color = textOff;
-		floorItText.GetComponent<Text> ().enabled = false;
-		playBowlingButton.GetComponent<Button> ().enabled = false;
-		playBowlingButton.GetComponent<Image> ().color = buttonOff;
-		bowlingText.GetComponent<Text> ().color = textOff;
-		bowlingText.GetComponent<Text> ().enabled = false;
-		playDriveButton.GetComponent<Button> ().enabled = false;
-		playDriveButton.GetComponent<Image> ().color = buttonOff;
-		driveText.GetComponent<Text> ().color = textOff;
-		driveText.GetComponent<Text> ().enabled = false;
-
-		highScore.GetComponent<Text> ().color = textOff;
-		bowlingHighScore.GetComponent<Text> ().color = textOff;
-		drivingHighScore.GetComponent<Text> ().color = textOff;
-
-		soundButton.GetComponent<Button> ().enabled = true;
-		soundButton.GetComponent<Image> ().color = buttonOn;
-		soundText.GetComponent<Text> ().color = textOn;
-		musicButton.GetComponent<Button> ().enabled = true;
-		musicButton.GetComponent<Image> ().color = buttonOn;
-		musicText.GetComponent<Text> ().color = textOn;
-		vibrationButton.GetComponent<Button> ().enabled = true;
-		vibrationButton.GetComponent<Image> ().color = buttonOn;
-		vibrationText.GetComponent<Text> ().color = textOn;
-	}
-
-	void settingsOff(){
+		
+	void menuOn(){
+		turnOffAll ();
+		title.text = "Floor It";
 		playButton.GetComponent<Button> ().enabled = true;
 		playButton.GetComponent<Image> ().color = buttonOn;
 		floorItText.GetComponent<Text> ().color = textOn;
@@ -140,15 +115,91 @@ public class MainMenu : MonoBehaviour {
 		bowlingHighScore.GetComponent<Text> ().color = textOn;
 		drivingHighScore.GetComponent<Text> ().color = textOn;
 
+		storeButton.GetComponent<Button> ().enabled = true;
+		storeButton.GetComponent<Image> ().color = buttonOn;
+		storeText.GetComponent<Text> ().color = textOn;
+		settingsButton.GetComponent<Button> ().enabled = true;
+		settingsButton.GetComponent<Image> ().color = buttonOn;
+		settingsText.GetComponent<Text> ().color = textOn;
+	}
+
+	void settingsOn(){
+		//PlayerPrefs.DeleteAll();
+		turnOffAll();
+		title.text = "Settings";
+		settingsButton.GetComponent<Button> ().enabled = true;
+		settingsButton.GetComponent<Image> ().color = buttonOn;
+		settingsText.GetComponent<Text> ().color = textOn;
+
+		soundButton.GetComponent<Button> ().enabled = true;
+		soundButton.GetComponent<Image> ().color = buttonOn;
+		soundText.GetComponent<Text> ().enabled = true;
+		soundText.GetComponent<Text> ().color = textOn;
+		musicButton.GetComponent<Button> ().enabled = true;
+		musicButton.GetComponent<Image> ().color = buttonOn;
+		musicText.GetComponent<Text> ().enabled = true;
+		musicText.GetComponent<Text> ().color = textOn;
+		vibrationButton.GetComponent<Button> ().enabled = true;
+		vibrationButton.GetComponent<Image> ().color = buttonOn;
+		vibrationText.GetComponent<Text> ().enabled = true;
+		vibrationText.GetComponent<Text> ().color = textOn;
+	}
+
+	void storeOn(){
+		turnOffAll ();
+		title.text = "Store";
+		storeButton.GetComponent<Button> ().enabled = true;
+		storeButton.GetComponent<Image> ().color = buttonOn;
+		storeText.GetComponent<Text> ().color = textOn;
+
+		sudanButton.GetComponent<Button> ().enabled = true;
+		sudanButton.GetComponent<Image> ().color = textOn;
+		limoButton.GetComponent<Button> ().enabled = true;
+		limoButton.GetComponent<Image> ().color = textOn;
+	}
+
+	void turnOffAll(){
+		playButton.GetComponent<Button> ().enabled = false;
+		playButton.GetComponent<Image> ().color = buttonOff;
+		floorItText.GetComponent<Text> ().color = textOff;
+		floorItText.GetComponent<Text> ().enabled = false;
+		playBowlingButton.GetComponent<Button> ().enabled = false;
+		playBowlingButton.GetComponent<Image> ().color = buttonOff;
+		bowlingText.GetComponent<Text> ().color = textOff;
+		bowlingText.GetComponent<Text> ().enabled = false;
+		playDriveButton.GetComponent<Button> ().enabled = false;
+		playDriveButton.GetComponent<Image> ().color = buttonOff;
+		driveText.GetComponent<Text> ().color = textOff;
+		driveText.GetComponent<Text> ().enabled = false;
+
+		highScore.GetComponent<Text> ().color = textOff;
+		bowlingHighScore.GetComponent<Text> ().color = textOff;
+		drivingHighScore.GetComponent<Text> ().color = textOff;
+
+		storeButton.GetComponent<Button> ().enabled = false;
+		storeButton.GetComponent<Image> ().color = buttonOff;
+		storeText.GetComponent<Text> ().color = textOff;
+		settingsButton.GetComponent<Button> ().enabled = false;
+		settingsButton.GetComponent<Image> ().color = buttonOff;
+		settingsText.GetComponent<Text> ().color = textOff;
+
 		soundButton.GetComponent<Button> ().enabled = false;
 		soundButton.GetComponent<Image> ().color = buttonOff;
+		soundText.GetComponent<Text> ().enabled = false;
 		soundText.GetComponent<Text> ().color = textOff;
 		musicButton.GetComponent<Button> ().enabled = false;
 		musicButton.GetComponent<Image> ().color = buttonOff;
+		musicText.GetComponent<Text> ().enabled = false;
 		musicText.GetComponent<Text> ().color = textOff;
 		vibrationButton.GetComponent<Button> ().enabled = false;
 		vibrationButton.GetComponent<Image> ().color = buttonOff;
+		vibrationText.GetComponent<Text> ().enabled = false;
 		vibrationText.GetComponent<Text> ().color = textOff;
+
+		sudanButton.GetComponent<Button> ().enabled = false;
+		sudanButton.GetComponent<Image> ().color = buttonOff;
+		limoButton.GetComponent<Button> ().enabled = false;
+		limoButton.GetComponent<Image> ().color = buttonOff;
 	}
 
 	public void playButtonClick() {
@@ -184,6 +235,21 @@ public class MainMenu : MonoBehaviour {
 	public void settingsButtonClick () {
 		Camera.main.GetComponent<SoundEffects> ().playButtonClick ();
 		viewSettings = !viewSettings;
+		if (viewSettings) {
+			settingsOn ();
+		} else {
+			menuOn ();
+		}
+	}
+
+	public void storeButtonClick () {
+		Camera.main.GetComponent<SoundEffects> ().playButtonClick ();
+		viewStore = !viewStore;
+		if (viewStore) {
+			storeOn ();
+		} else {
+			menuOn ();
+		}
 	}
 
 	public void soundEffectsButtonClick() {
@@ -224,6 +290,20 @@ public class MainMenu : MonoBehaviour {
 			vibrationText.text = "Vibration: On";
 			Camera.main.GetComponent<SoundEffects> ().playMenuMusic ();
 		}
+		PlayerPrefs.Save ();
+	}
+
+	public void sudanButtonClick() {
+		Camera.main.GetComponent<SoundEffects> ().playButtonClick ();
+		GameObject.Find ("Car").GetComponent<MeshFilter> ().mesh = carMeshes[0];
+		PlayerPrefs.SetInt ("Car Number", 0);
+		PlayerPrefs.Save ();
+	}
+
+	public void limoButtonClick() {
+		Camera.main.GetComponent<SoundEffects> ().playButtonClick ();
+		GameObject.Find ("Car").GetComponent<MeshFilter> ().mesh = carMeshes[1];
+		PlayerPrefs.SetInt ("Car Number", 1);
 		PlayerPrefs.Save ();
 	}
 
